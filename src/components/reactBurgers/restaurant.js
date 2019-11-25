119cec70da14814efc3b13b2eded2aa68cc49027
Paul Ayling <36618373+PaulAyling@users.noreply.github.com>
2019-11-25 07:30:27 +0700

    backup
    gjk

--- src/components/reactBurgers/restaurant.js
+++ src/components/reactBurgers/restaurant.js
@@ -2,13 +2,15 @@ import React, { Fragment } from "react";
 // custom CSS
 import "../../css/components/reactBurgerShop.css";
 // custom components
+import CodeContainer from './codeContainer'
+
 import Fridge from "../reactBurgers/fridge";
 import FengShui from "../reactBurgers/fengShui";
 import window from "../reactBurgers/window";
 
 import Menu from "../reactBurgers/menu";
 
-import Burger from "../reactBurgers/burger";
+// import Burger from "../reactBurgers/burger";
 import Shelf from "../reactBurgers/shelf";
 
 const ReactResteraunt = () => {
@@ -17,6 +19,7 @@ const ReactResteraunt = () => {
       <h1>I am ready ...... What is your order.... supersize that?</h1>
       <div className="react-burger_assets">
         <FengShui />
+        <CodeContainer/>
       </div>
     </Fragment>
   );
